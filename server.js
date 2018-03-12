59886640fc114660b498670ba3642fd399286265
riccardonuzz <Riccardonuzz@yahoo.it>
2018-03-12 20:39:35 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//Middlewares
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error)
+            console.log('Unable to write on server.log.');
+    });
+
+    next();
+});
+
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+//Helpers
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+
+
+//Routes
+
+app.get('/', (req, res) => {
+    //res.send('Hello express!');
+    // res.send({
+    //     name: 'Riccardo',
+    //     likes: [
+    //         'Programming',
+    //         'Politic'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website!',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port :3000');
+});
