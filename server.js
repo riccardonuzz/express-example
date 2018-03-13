8b889a3b92ea8cb3ee9c4b538dc37e896add30fe
riccardonuzz <Riccardonuzz@yahoo.it>
2018-03-13 18:05:24 +0100

    Setup start scrpit and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -23,9 +24,9 @@ app.use((req, res, next) => {
 });
 
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -78,6 +79,6 @@ app.get('/bad', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up and running on port :3000');
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
 });
